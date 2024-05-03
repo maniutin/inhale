1a1bf3e091a6a193485854e919cccff8e52f48c3
Nick <maniutin@gmail.com>
2024-05-03 15:37:26 -0400

    trigger sound at equal lenghts

--- src/SynthVoice.tsx
+++ src/SynthVoice.tsx
@@ -4,11 +4,17 @@ const synth = new Tone.MonoSynth({
   oscillator: { type: "sine" },
 }).toDestination();
 
-const playSynth = () => {
-  synth.triggerAttackRelease("C5", "2n");
-};
-
 function SynthVoice() {
+  const playSynth = () => {
+    // synth.triggerAttackRelease("C5", "1n");
+
+    new Tone.Loop(() => {
+      synth.triggerAttackRelease("C4", 1);
+    }, 2).start();
+
+    Tone.Transport.start();
+    Tone.start();
+  };
   return <button onClick={playSynth}>Play</button>;
 }
 
